a3ba82e245006336f2908868764547fe56a3bb85
usmonjon <umediagram@gmail.com>
2024-06-05 00:47:21 +0500

    upload metter

---
+++ matter.js
@@ -0,0 +1,60 @@
+// masala 1
+/* const pInput = +prompt("Kvadratning tomonini kiriting");
+const p = pInput * 4;
+alert(`Kvadratning perimetri: ${p} ga teng`); */
+
+// masala 2
+/* const sInput = +prompt("Kvadratning tomonini kiriting");
+const s = sInput * sInput;
+alert(`Kvadratning yuzi: ${s} ga teng`); */
+
+// masala 3
+/* const rectangleA = +prompt("To'g'ri to'rtburchaknintg A tomonini kiriting");
+const rectangleB = +prompt("To'g'ri to'rtburchaknintg B tomonini kiriting");
+
+const rectangleS = rectangleA * rectangleB;
+const rectangleP = (rectangleA + rectangleB) * 2;
+
+alert(
+    `To'g'ri to'rtburchaknintg yuzi: ${rectangleS}\n\nuning perimetri: ${rectangleP} ga teng`,
+); */
+
+// masala 4
+/* const dInput = +prompt("Aylana diametrini kiritng");
+const pi = 3.14;
+const d = dInput * pi;
+alert(d); */
+
+// masala 5
+/* const cubeInput = +prompt("Kubning yon tomonini kiriting");
+const cubeV = cubeInput * cubeInput * cubeInput;
+const cubeS = 6 * cubeInput * cubeInput;
+alert(`V = ${cubeV}`);
+alert(`S = ${cubeS}`); */
+
+// masala 6
+/* const tomonA = prompt("Parallelepepidning a tomonini kiriting");
+const tomonB = prompt("Parallelepepidning b tomonini kiriting");
+const tomonC = prompt("Parallelepepidning c tomonini kiriting");
+
+const parallV = tomonA * tomonB * tomonC;
+const parallS = 2*((tomonA *  tomonB) + (tomonB * tomonC) + (tomonA * tomonC));
+
+alert(`Parallelepepidning hajmi: ${parallV}\n\nParallelepepidning to'la sirti: ${parallS}`); */
+
+// masala 7
+/* const radiusInput = prompt("Doiraning radiusini kiriting");
+const piForMatter7 = 3.14;
+const circleL = 2 * piForMatter7 * radiusInput;
+const circleS = piForMatter7 * radiusInput * radiusInput;
+alert(`Doiraning uzunligi: ${circleL}\n\nDoiraning yuzasi: ${circleS}`); */
+
+// masala 8
+/* const kiritishA = prompt("b sonini kiriting");
+const kiritishB = prompt("b sonini kiriting");
+
+const javob = (kiritishA + kiritishB) * 2;
+
+alert(
+    `${kiritishA} va ${kiritishB} sonlarining o'rta arifmetigi: ${javob} ga teng`,
+); */
